529bb97abfff7e7fae5e7761a567cf85d7eaad38
kisunan.lee <kisunan.lee@kisunanleeui-MacBookPro.local>
2023-03-14 17:47:20 +0900

    study: Part1~8

--- src/main/java/io/pivotal/literx/Part08OtherOperations.java
+++ src/main/java/io/pivotal/literx/Part08OtherOperations.java
@@ -17,21 +17,21 @@ public class Part08OtherOperations {
 
 	// TODO Create a Flux of user from Flux of username, firstname and lastname.
 	Flux<User> userFluxFromStringFlux(Flux<String> usernameFlux, Flux<String> firstnameFlux, Flux<String> lastnameFlux) {
-		return null;
+		return Flux.zip(usernameFlux, firstnameFlux, lastnameFlux).map(tuple -> new User(tuple.getT1(), tuple.getT2(), tuple.getT3()));
 	}
 
 //========================================================================================
 
 	// TODO Return the mono which returns its value faster
 	Mono<User> useFastestMono(Mono<User> mono1, Mono<User> mono2) {
-		return null;
+		return Mono.firstWithValue(mono1, mono2);
 	}
 
 //========================================================================================
 
 	// TODO Return the flux which returns the first value faster
 	Flux<User> useFastestFlux(Flux<User> flux1, Flux<User> flux2) {
-		return null;
+		return Flux.firstWithValue(flux1, flux2);
 	}
 
 //========================================================================================
