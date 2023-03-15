da5f1604c12d92025140fa461aa8667415b0a791
kisunan.lee <kisunan.lee@kisunanleeui-MacBookPro.local>
2023-03-15 13:36:14 +0900

    study: part8~11

--- src/main/java/io/pivotal/literx/Part08OtherOperations.java
+++ src/main/java/io/pivotal/literx/Part08OtherOperations.java
@@ -38,28 +38,28 @@ public class Part08OtherOperations {
 
 	// TODO Convert the input Flux<User> to a Mono<Void> that represents the complete signal of the flux
 	Mono<Void> fluxCompletion(Flux<User> flux) {
-		return null;
+		return flux.then();
 	}
 
 //========================================================================================
 
 	// TODO Return a valid Mono of user for null input and non null input user (hint: Reactive Streams do not accept null values)
 	Mono<User> nullAwareUserToMono(User user) {
-		return null;
+		return Mono.justOrEmpty(user);
 	}
 
 //========================================================================================
 
 	// TODO Return the same mono passed as input parameter, expect that it will emit User.SKYLER when empty
 	Mono<User> emptyToSkyler(Mono<User> mono) {
-		return null;
+		return mono.defaultIfEmpty(User.SKYLER);
 	}
 
 //========================================================================================
 
 	// TODO Convert the input Flux<User> to a Mono<List<User>> containing list of collected flux values
 	Mono<List<User>> fluxCollection(Flux<User> flux) {
-		return null;
+		return flux.collectList();
 	}
 
 }
